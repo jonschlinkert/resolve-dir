3e076a9a0710d5ed4493b4cc3620a4e0dd5a84c4
Blaine Bublitz <blaine.bublitz@gmail.com>
2017-08-05 16:34:17 -0700

    Use path.join to achieve passing tests on Windows

--- test.js
+++ test.js
@@ -8,6 +8,7 @@
 'use strict';
 
 require('mocha');
+var path = require('path');
 var homedir = require('homedir-polyfill');
 var assert = require('assert');
 var gm = require('global-modules');
@@ -20,11 +21,11 @@ describe('resolve', function() {
 
   it('should resolve the path to user home:', function() {
     assert.equal(resolve('~'), homedir());
-    assert.equal(resolve('~/foo'), homedir() + '/foo');
+    assert.equal(resolve('~/foo'), path.join(homedir(), '/foo'));
   });
 
   it('should resolve the path to global npm modules:', function() {
     assert.equal(resolve('@'), gm);
-    assert.equal(resolve('@/foo'), gm + '/foo');
+    assert.equal(resolve('@/foo'), path.join(gm, '/foo'));
   });
 });
